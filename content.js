09626476b10f91451530865dd2bd977cde0a9c8c
Artem Ilchenko <artemil@wizardsdev.com>
2021-04-10 08:44:31 +0300

    add url for create new pr; remove auto add icons

--- content.js
+++ content.js
@@ -1,28 +1,5 @@
 (function () {
   const $titleInput = document.getElementById("id_title");
-  const $fromBranch = document.querySelector(".select2-chosen");
-
-  const STATUSES = {
-    WFER: '⚡',
-    WFR: '🍭',
-    WFMR: '🧸',
-    WFB: '🚧'
-  }
-
-  const fromBranch = $fromBranch.textContent;
-  const initTitleValue = $titleInput.value;
-  let newTitleValue = initTitleValue.replace(/.*:\s?/i, '');
-
-  if (
-    (fromBranch.startsWith('main/') || fromBranch.startsWith('bug/'))
-    && !newTitleValue.includes(STATUSES.WFER)) {
-    newTitleValue = STATUSES.WFER + newTitleValue
-  }
-
-  if (initTitleValue !== newTitleValue) {
-    $titleInput.value = newTitleValue
-  }
-
 
   chrome.runtime.onMessage.addListener(
     function(request) {
